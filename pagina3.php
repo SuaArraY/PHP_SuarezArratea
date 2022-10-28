c0644e0a9e3ff0adb8116b333bb70cc34bce4a1b
SuaArraY <1313178@gmail.com>
2022-10-28 12:43:57 -0500

    parte2

---
+++ pagina3.php
@@ -0,0 +1,7 @@
+
+<?php
+
+echo "<h1>hola, desde PHP</h1>";
+echo "<p>Lorem ipsum es el texto que se usa habitualmente en diseño gráfico en demostraciones de tipografías o de borradores de diseño </p>";
+
+?>
